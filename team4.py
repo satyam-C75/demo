4d47c9cb34a00795eaa0e4841f6f19002d2244ec
Peeyush Sinha <peeyushsinha238@gmail.com>
2024-09-10 12:43:22 +0530

    Peeyush Commit

--- team4.py
+++ team4.py
@@ -3,6 +3,16 @@ def printa(a):
     for i in 10:
         print(a[i])
 
+#Peeyush 
+#adding 10 numbers
+def add_numbers(start, end):
+  total = 0
+  for num in range(start, end + 1):
+    total += num
+  return total
+
+sum_of_numbers = add_numbers(1, 10)
+print("Sum of numbers from 1 to 10:", sum_of_numbers)
 #rishabh
 import random
 
@@ -15,3 +25,4 @@ average = sum(random_numbers) / len(random_numbers)
 # Print the results
 print("Random Numbers:", random_numbers)
 print("Average:", average)
+
